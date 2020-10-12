b524e8488a40e18a1bd319bea834ade83744fa0c
g-osia <gosiex@wp.eu>
2020-10-12 13:42:45 +0200

    welcome

--- src/App.js
+++ src/App.js
@@ -38,7 +38,11 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Week 2</h1>
+
+      <h1>Welcome to Codaisseur</h1>
+
+      <p>After the long week 1 with JavaScript, now is the time for:</p>
+      <h2>Week 2</h2>
 
       <p>
         Still want to explore React more!!!
