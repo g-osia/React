5057003b911d10819362950aab2e35122d1cf181
g-osia <gosiex@wp.eu>
2020-10-12 13:47:04 +0200

    welcome

--- src/App.js
+++ src/App.js
@@ -1,40 +1,5 @@
 import React from 'react';
 
-
-// function Hello() {
-//   return <h1>Hello!</h1>
-// }
-
-// function Greeting({name, age, country}) {
-
-//   const message = age
-//   ? `You are ${age}.`
-//   : "I don't know your age."
-
-//   const origin = country
-//   ?`You are from ${country}.`
-//   : 'I don\'t know where you are from.'
-
-//   return <div>
-//     <Hello/>
-//     {name}! { }
-//     {message} { }
-//     {origin}
-//   </div>
-// }
-
-// function App() {
-//   return (
-//    <div>
-//       <Greeting name = 'Gosia' age = {32} country = 'Poland'/>
-//       <Greeting name = 'Dori'/>
-
-//    </div>
-// )
-// }
-
-
-
 function App() {
   return (
     <div>
